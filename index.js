258afe451e2479388c09c46652d6069b0254a2ff
Mark Buyco <markabadbuyco@gmail.com>
2024-01-08 22:33:45 +0400

    Commit for all
    One commit for everything

---
+++ index.js
@@ -0,0 +1,135 @@
+//buttons links for Community.html, will be directed to a different page when clicked
+
+function visitElliotPage() {
+    window.location.href = "https://www.youtube.com/@elliotpage_";
+}
+
+function visitChance() {
+    window.location.href = "https://www.youtube.com/@ChanceDubinick";
+}
+
+function visitConcepts() {
+    window.location.href = "https://cncpts.ae/";
+}
+
+function visitStockX() {
+    window.location.href = "https://stockx.com/";
+}
+
+function visitPod1() {
+    window.location.href = "https://podcasts.apple.com/us/podcast/sneaker-history-podcast-sneakers-sneaker-culture-and/id1456480933";
+}
+
+function visitPod2() {
+    window.location.href = "https://podcasts.apple.com/us/podcast/the-complex-sneakers-show/id1487701689";
+}
+
+//images links on New releases, will be redirected to another site to be able to buy the shoe
+
+function buyshoe1() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-air-force-1-07-coconut-milk-dm0211-101/460819?channel=Widget";
+}
+
+function buyshoe2() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-dunk-low-wmns-nba-paris-fz4624-001/463180?channel=Widget";
+}
+
+function buyshoe3() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-air-max-90-gtx-bright-crimson-fd5810-101/445931?channel=Widget";
+}
+
+function buyshoe4() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-1-retro-high-og-lost-found-dz5485-612/347638?channel=Widget";
+}
+
+function buyshoe5() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-4-retro-military-black-dh6927-111/293328?channel=Widget";
+}
+
+function buyshoe6() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-4-retro-white-midnight-navy-dh6927-140/341670?channel=Widget";
+}
+
+function buyshoe7() {
+    window.location.href = "https://www.sneakerjagers.com/s/a-ma-maniere-x-air-jordan-4-violet-ore-dv6773-220/358202?channel=Widget";
+}
+
+function buyshoe8() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-4-retro-sw-black-canvas-dh7138-006/338746?channel=Widget";
+}
+
+function buyshoe9() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-4-zen-master/246347?channel=Widget";
+}
+
+function buyshoe10() {
+    window.location.href = "https://www.sneakerjagers.com/s/adidas-handball-spezial-wmns-grey-black-gum-if6562/425058?channel=Widget";
+}
+
+function buyshoe11() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-sb-dunk-low-deep-royal-blue-hf3704-400/463177?channel=Widget";
+}
+
+function buyshoe12() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-1-retro-high-og-yellow-ochre-dz5485-701/416270?channel=Widget";
+}
+
+function buyshoe13() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-3-midnight-navy-ct8532-140/416268?channel=Widget";
+}
+
+function buyshoe14() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-air-footscape-phantom-earth-fz4340-100/449018?channel=Widget";
+}
+
+function buyshoe15() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-wmns-air-max-90-og-rose-cd0490-102/126016?channel=Widget";
+}
+
+function buyshoe16() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-air-max-1-87-dz2628-104/443365?channel=Widget";
+}
+
+function buyshoe17() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-air-force-1-07-prm-canvas-light-cream-ci9349-200/94131?channel=Widget";
+}
+
+function buyshoe18() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-dunk-low-valerian-blue-dd1391-400/294262?channel=Widget";
+}
+
+function buyshoe19() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-air-max-1-valentines-day-204-fz4346-200/463027?channel=Widget";
+}
+
+function buyshoe20() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-dunk-low-wmns-valentines-day-fq7056-100/454636?channel=Widget";
+}
+
+function buyshoe21() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-1-chinese-new-year-2024-fn3727-100/416251?channel=Widget";
+}
+
+function buyshoe22() {
+    window.location.href = "https://www.sneakerjagers.com/s/nike-sb-dunk-low-court-purple-bq6817-500/185493?channel=Widget";
+}
+
+function buyshoe23() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-6-yellow-ochre-ct8529-170/416264?channel=Widget";
+}
+
+function buyshoe24() {
+    window.location.href = "https://www.sneakerjagers.com/s/air-jordan-spizike-low-houston-oilers-fq1759-400/463182?channel=Widget";
+}
+
+//Shoe Stories, go to page destination, the links are on the html now, instead in javascript.
+
+function goToShoePage(destination) {
+    window.location.href = destination;
+}
+
+// Gallery Redirect, will be redirect to another site.
+
+function redirectTo(url) {
+    window.location.href = url;
+}
